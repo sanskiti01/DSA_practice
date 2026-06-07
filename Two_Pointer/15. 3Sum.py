65e10e29025d19a10dfa26473121820ee290ff98
Sankriti <sanskritikant2@gmail.com>
2026-06-07 23:47:43 +0530

    y

--- Two_Pointer/15. 3Sum.py
+++ Two_Pointer/15. 3Sum.py
@@ -1,33 +1,37 @@
-def Threesum(nums):
-    nums.sort()
-    res = []
+def threeSum(nums):
+        nums.sort()
+        result = []
 
-    for i in range(len(nums)):
-        if i > 0 and nums[i] == nums[i-1]:
-            continue
+        for i in range(len(nums)):
 
-        left = i+1
-        right = len(nums)-1
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-        while left < right:
-            total = nums[i]+nums[left]+nums[right]
+            left = i + 1
+            right = len(nums) - 1
 
-            if total>0:
-                right-=1
-            elif total<0:
-                left+=1
-            else:
-                res.append([nums[i],nums[left],nums[right]])
-                left+=1
-                right-=1
+            while left < right:
 
-                while left< right and nums[left] == nums[left-1]:
-                    left+=1
+                total = nums[i] + nums[left] + nums[right]
 
-    return res
+                if total < 0:
+                    left += 1
 
-print(Threesum([0,0,0]))    
+                elif total > 0:
+                    right -= 1
 
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
 
+                    left += 1
+                    right -= 1
 
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
 
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return result
+
+print(threeSum([0,0,0]))
